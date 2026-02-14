d15c1244c565d77dad6847a75fce7645433b5cb8
Rahul Kissoon <rahulkissoon07@gmail.com>
2026-02-14 02:11:42 -0400

    feat: separate data files

--- core.c
+++ core.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <conio.h>
+#include <direct.h>
 #include "accounting.h"
 #include "auth.h"
 #include "core.h"
@@ -10,18 +11,26 @@
 #include "clubs.h"
 #include "students.h"
 
-const char *DATA_FILENAME = "data.bin";
-
 char school_name[MAX_SCHOOL_NAME_LENGTH] = "";
 
 // save_data_to_file ensures that data persists across sessions.
 // Once the process safely exits, data can be reloaded from the file during the next session.
 void save_data_to_file()
 {
-    FILE *datafile_ptr = fopen(DATA_FILENAME, "wb");
-    while (datafile_ptr == NULL)
+    // Checks if data directory exists. If none exists, creates directory.
+    _mkdir("data");
+
+    // Opens the datafiles in write and binary mode
+    FILE *core_datafile_ptr = fopen("data/core.bin", "wb");
+    FILE *accounting_datafile_ptr = fopen("data/accounting.bin", "wb");
+    FILE *clubs_datafile_ptr = fopen("data/clubs.bin", "wb");
+    FILE *meetings_datafile_ptr = fopen("data/meetings.bin", "wb");
+    FILE *students_datafile_ptr = fopen("data/students.bin", "wb");
+
+    while (core_datafile_ptr == NULL || accounting_datafile_ptr == NULL || clubs_datafile_ptr == NULL || meetings_datafile_ptr == NULL || students_datafile_ptr == NULL)
     {
-        printf("Error in saving data to '%s'. Press [Q] to quit without saving data (not recommended) or any other key to try again.\n", DATA_FILENAME);
+        clear_console();
+        printf("Error in saving data to datafiles. Press [Q] to quit without saving data (not recommended) or any other key to try again.\n");
         char decision_on_err = _getch(); // In the event that the file cannot be opened, the user can choose to quit the program or try again.
         if (tolower(decision_on_err) == 'q')
         {
@@ -30,83 +39,103 @@ void save_data_to_file()
         }
         else
         {
-            clear_console();
-            datafile_ptr = fopen(DATA_FILENAME, "wb"); // Opens the file in write and binary mode
+            core_datafile_ptr = fopen("data/core.bin", "wb");
+            accounting_datafile_ptr = fopen("data/accounting.bin", "wb");
+            clubs_datafile_ptr = fopen("data/clubs.bin", "wb");
+            meetings_datafile_ptr = fopen("data/meetings.bin", "wb");
+            students_datafile_ptr = fopen("data/students.bin", "wb");
         }
     }
 
-    fwrite(&school_name, sizeof(char), MAX_SCHOOL_NAME_LENGTH, datafile_ptr);
+    fwrite(&school_name, sizeof(char), MAX_SCHOOL_NAME_LENGTH, core_datafile_ptr);
 
     int admin_password_length = strlen(admin_password) + 1;
-    fwrite(&admin_password_length, sizeof(int), 1, datafile_ptr);              // The admin password is of variable length so its length must be recorded.
-    fwrite(admin_password, sizeof(char), admin_password_length, datafile_ptr); // Saves the admin password
+    fwrite(&admin_password_length, sizeof(int), 1, core_datafile_ptr);              // The admin password is of variable length so its length must be recorded.
+    fwrite(admin_password, sizeof(char), admin_password_length, core_datafile_ptr); // Saves the admin password
 
-    fwrite(&club_count, sizeof(int), 1, datafile_ptr);   // Saves the club count
-    fwrite(&prev_club_id, sizeof(int), 1, datafile_ptr); // Saves the previous club ID
-    for (int i = 0; i < club_count; i++)                 // Saves all club info
+    fwrite(&club_count, sizeof(int), 1, clubs_datafile_ptr);   // Saves the club count
+    fwrite(&prev_club_id, sizeof(int), 1, clubs_datafile_ptr); // Saves the previous club ID
+    for (int i = 0; i < club_count; i++)                       // Saves all club info
     {
-        fwrite(&clubs[i], sizeof(struct Club), 1, datafile_ptr);
-        fwrite(clubs[i].password, sizeof(char), clubs[i].password_length, datafile_ptr);
+        fwrite(&clubs[i], sizeof(struct Club), 1, clubs_datafile_ptr);
+        fwrite(clubs[i].password, sizeof(char), clubs[i].password_length, clubs_datafile_ptr);
     }
 
     for (int i = 0; i < transaction_count; i++) // Saves all transactions
     {
-        fwrite(&transactions[i], sizeof(struct Transaction), 1, datafile_ptr);
+        fwrite(&transactions[i], sizeof(struct Transaction), 1, accounting_datafile_ptr);
     }
 
     for (int i = 0; i < meeting_count; i++) // Saves all club meetings
     {
-        fwrite(&meetings[i], sizeof(struct Meeting), 1, datafile_ptr);
+        fwrite(&meetings[i], sizeof(struct Meeting), 1, meetings_datafile_ptr);
     }
 
-    fwrite(&student_count, sizeof(int), 1, datafile_ptr);   // Saves the student count
-    fwrite(&prev_student_id, sizeof(int), 1, datafile_ptr); // Saves the previous student ID
-    for (int i = 0; i < student_count; i++)                 // Saves all student info
+    fwrite(&student_count, sizeof(int), 1, students_datafile_ptr);   // Saves the student count
+    fwrite(&prev_student_id, sizeof(int), 1, students_datafile_ptr); // Saves the previous student ID
+    for (int i = 0; i < student_count; i++)                          // Saves all student info
     {
-        fwrite(&students[i], sizeof(struct Student), 1, datafile_ptr);
+        fwrite(&students[i], sizeof(struct Student), 1, students_datafile_ptr);
     }
 
-    fclose(datafile_ptr); // Closes the data file
+    // Closes the datafiles
+    fclose(core_datafile_ptr);
+    fclose(accounting_datafile_ptr);
+    fclose(clubs_datafile_ptr);
+    fclose(meetings_datafile_ptr);
+    fclose(students_datafile_ptr);
 }
 
 // load_data_from_file reads the database, loading all data into the active memory of the program.
 void load_data_from_file()
 {
-    FILE *datafile_ptr = fopen(DATA_FILENAME, "rb"); // Opens the file in read and binary mode
+    // Opens the datafiles in read and binary mode
+    FILE *core_datafile_ptr = fopen("data/core.bin", "rb");
+    FILE *accounting_datafile_ptr = fopen("data/accounting.bin", "rb");
+    FILE *clubs_datafile_ptr = fopen("data/clubs.bin", "rb");
+    FILE *meetings_datafile_ptr = fopen("data/meetings.bin", "rb");
+    FILE *students_datafile_ptr = fopen("data/students.bin", "rb");
+
     // If the file does not exist, that means that this is the first time the program is being run.
     // Active memory with default values will be used and stored later.
-    if (datafile_ptr == NULL)
+
+    if (core_datafile_ptr != NULL)
     {
-        return;
-    }
-    else
-    {
-        fread(&school_name, sizeof(char), MAX_SCHOOL_NAME_LENGTH, datafile_ptr); // Reads the school name
+        fread(&school_name, sizeof(char), MAX_SCHOOL_NAME_LENGTH, core_datafile_ptr); // Reads the school name
 
         int admin_password_length = 0;
-        fread(&admin_password_length, sizeof(int), 1, datafile_ptr);
+        fread(&admin_password_length, sizeof(int), 1, core_datafile_ptr);
         admin_password = malloc(admin_password_length * sizeof(char)); // Allocates enough memory based on the length of the admin password
-        fread(admin_password, sizeof(char), admin_password_length, datafile_ptr);
+        fread(admin_password, sizeof(char), admin_password_length, core_datafile_ptr);
+    }
 
-        fread(&club_count, sizeof(int), 1, datafile_ptr);   // Reads the club count
-        fread(&prev_club_id, sizeof(int), 1, datafile_ptr); // Reads the previous club ID
-        for (int i = 0; i < club_count; i++)                // Reads all club info
+    if (clubs_datafile_ptr != NULL)
+    {
+        fread(&club_count, sizeof(int), 1, clubs_datafile_ptr);   // Reads the club count
+        fread(&prev_club_id, sizeof(int), 1, clubs_datafile_ptr); // Reads the previous club ID
+        for (int i = 0; i < club_count; i++)                      // Reads all club info
         {
-            fread(&clubs[i], sizeof(struct Club), 1, datafile_ptr);
+            fread(&clubs[i], sizeof(struct Club), 1, clubs_datafile_ptr);
             clubs[i].password = malloc(clubs[i].password_length * sizeof(char)); // Allocates enough memory based on the length of the club password
-            fread(clubs[i].password, sizeof(char), clubs[i].password_length, datafile_ptr);
+            fread(clubs[i].password, sizeof(char), clubs[i].password_length, clubs_datafile_ptr);
 
             meeting_count += clubs[i].meeting_count;         // Increments the global meeting count based on meeting count of the club
             transaction_count += clubs[i].transaction_count; // Increments the global transaction count based on transaction count of the club
         }
+    }
 
-        // The reason for the ternary operator is to prevent allocating 0 bytes of memory. This can cause undefined behaviour in C.
+    // The reason for the ternary operator is to prevent allocating 0 bytes of memory. This can cause undefined behaviour in C.
+    if (accounting_datafile_ptr != NULL)
+    {
         transactions = malloc((transaction_count == 0 ? 1 : transaction_count) * sizeof(struct Transaction));
         for (int i = 0; i < transaction_count; i++) // Reads all transactions
         {
-            fread(&transactions[i], sizeof(struct Transaction), 1, datafile_ptr);
+            fread(&transactions[i], sizeof(struct Transaction), 1, accounting_datafile_ptr);
         }
+    }
 
+    if (meetings_datafile_ptr != NULL)
+    {
         meetings = malloc((meeting_count == 0 ? 1 : meeting_count) * sizeof(struct Meeting));
         if (meetings == NULL)
         {
@@ -114,11 +143,14 @@ void load_data_from_file()
         }
         for (int i = 0; i < meeting_count; i++)
         {
-            fread(&meetings[i], sizeof(struct Meeting), 1, datafile_ptr);
+            fread(&meetings[i], sizeof(struct Meeting), 1, meetings_datafile_ptr);
         }
+    }
 
-        fread(&student_count, sizeof(int), 1, datafile_ptr);   // Reads the student count
-        fread(&prev_student_id, sizeof(int), 1, datafile_ptr); // Reads the previous student ID
+    if (students_datafile_ptr != NULL)
+    {
+        fread(&student_count, sizeof(int), 1, students_datafile_ptr);   // Reads the student count
+        fread(&prev_student_id, sizeof(int), 1, students_datafile_ptr); // Reads the previous student ID
         // The reason for the ternary operator is to prevent allocating 0 bytes of memory. This can cause undefined behaviour in C.
         students = malloc((student_count == 0 ? 1 : student_count) * sizeof(struct Student));
         if (students == NULL)
@@ -127,11 +159,16 @@ void load_data_from_file()
         }
         for (int i = 0; i < student_count; i++) // Reads all student info
         {
-            fread(&students[i], sizeof(struct Student), 1, datafile_ptr);
+            fread(&students[i], sizeof(struct Student), 1, students_datafile_ptr);
         }
     }
 
-    fclose(datafile_ptr);
+    // Closes the datafiles
+    fclose(core_datafile_ptr);
+    fclose(accounting_datafile_ptr);
+    fclose(clubs_datafile_ptr);
+    fclose(meetings_datafile_ptr);
+    fclose(students_datafile_ptr);
 }
 
 // first_time_setup is called when the program is run for the first time.
--- menu_system.h
+++ menu_system.h
@@ -8,6 +8,6 @@ void show_admin_panel();
 void prompt_return(char *to);
 void print_table(int total_columns, int total_rows, int *column_widths, int total_footer_rows, char *column_headings[], char ***contents, char *footer_rows[]);
 time_t parse_time_input();
-char *format_time_t();
+char *format_time_t(time_t timestamp);
 void report_alloc_error(long long int bytes);
 #endif
